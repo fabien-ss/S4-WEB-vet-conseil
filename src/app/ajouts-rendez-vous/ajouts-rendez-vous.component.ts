100bbba66e661af211c7c210c34f93278cb19d14
Safidymamin <118508017+Safidymamin@users.noreply.github.com>
2023-06-01 19:35:12 +0200

    feat : components

---
+++ src/app/ajouts-rendez-vous/ajouts-rendez-vous.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-ajouts-rendez-vous',
+  templateUrl: './ajouts-rendez-vous.component.html',
+  styleUrls: ['./ajouts-rendez-vous.component.css']
+})
+export class AjoutsRendezVousComponent {
+
+}
---
+++ src/app/gestion-rendez-vous/gestion-rendez-vous.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-gestion-rendez-vous',
+  templateUrl: './gestion-rendez-vous.component.html',
+  styleUrls: ['./gestion-rendez-vous.component.css']
+})
+export class GestionRendezVousComponent {
+
+}
